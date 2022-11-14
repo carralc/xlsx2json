c3b46244d2b1a7ac75784bc8a0faebde2642ebae
Carlos Carral <carlos.c@vauxoo.com>
2022-11-14 11:25:39 -0600

    Hacer un best effort para interpretar la fecha

--- main.py
+++ main.py
@@ -1,31 +1,41 @@
-import xlrd
 import pandas as pd
-from datetime import datetime
+import dateutil
+from dateutil.parser import ParserError
+import sys
 
 XLSX_PATH = "Ejemplo Excel.xlsx"
 
 
-def xlsx2json(path, sheet=0, skip=1):
-    """
-    Convierte una hoja de excel a json
-
-    :param sheet int: Número de hoja en la que se encuentran los datos 
-    :param skip int: Número de filas que va a ignorar antes de procesar los datos 
-    """
-    # NOTA: La lectura de XLSX se considera una vulnerabilidad en xlrd >= 2.0
-    # Más información al respecto aquí
-    # https://groups.google.com/g/python-excel/c/IRa8IWq_4zk/m/Af8-hrRnAgAJ?pli=1
-    # Sin embargo, la rama de odoo vauxoo/13.0 utiliza xlrd == '1.1.0'
-    workbook = xlrd.open_workbook(path)
-    sheet = workbook.sheet_by_index(sheet)
-
-    for row_idx in range(skip, sheet.nrows):
-        cell = sheet.cell(row_idx, 1)
-        cell.ctype = 3
-        #  print(cell.value)
+def warning(s):
+    print(f"WARNING: {s}", file=sys.stderr)
 
 
-def xlsx2jsonPandas(path):
+def error(s):
+    print(f"ERROR: {s}", file=sys.stderr)
+    sys.exit(-1)
+
+
+def check_date(s):
+    """Intenta hacer parse de una cadena de fecha y emite un warning
+    si no se puede acoplar al formato esperado"""
+    FORMAT = "%Y-%m-%d"
+
+    if 'datetime.datetime' in str(type(s)):
+        return s.strftime(FORMAT)
+    elif "string" in str(type(s)):
+        warning(
+            f"El objeto {s} no es una fecha. Intentando interpretar como fecha")
+        try:
+            # Hacer un parse en "best effort"
+            date = dateutil.parser.parse(str(s))
+            return date.strftime(FORMAT)
+        except ParserError:
+            error(
+                f"Imposible determinar una fecha de {s}. Deteniendo proceso.")
+            pass
+
+
+def xlsx2json(path):
     fields = ["purchase_order_id.partner_ref",
               "invoice_line_ids.name",
               "check_in_date",
@@ -46,23 +56,28 @@ def xlsx2jsonPandas(path):
               "invoice_line_ids.product_id",
               "is_refundable"]
 
-    def get_date(s): return datetime.strptime(s, "%d/%m/%Y")
     workbook = pd.read_excel(path,
                              # Usar solo las columnas que nos interesan
                              usecols=[0, 2, 3, 5, 6, 7, 8, 9, 10,
                                       11, 12, 13, 14, 15, 16, 17, 18, 19, 20],
                              # Usar los nombres del modelo
-                             names=fields)
+                             names=fields, parse_dates=False,
+                             converters={
+                                 "check_in_date": check_date,
+                                 "invoice_date": check_date,
+                                 "max_cancel_date": check_date})
 
-    pd.set_option('display.max_columns', None)
-    print(workbook)
+    #  pd.set_option('display.max_columns', None)
+    #  print(workbook)
     workbook["purchase_order_id"] = workbook.apply(lambda row: {
         "partner_ref": row["purchase_order_id.partner_ref"],
+        "currency_id": row["purchase_order_id.currency_id"],
         "price_unit": row["purchase_order_id.price_unit"],
         "partner_id": row["purchase_order_id.partner_id"]
     }, axis=1)
 
     workbook.drop(["purchase_order_id.partner_ref",
+                   "purchase_order_id.currency_id",
                    "purchase_order_id.price_unit",
                    "purchase_order_id.partner_id"], axis=1, inplace=True)
 
@@ -88,16 +103,8 @@ def xlsx2jsonPandas(path):
         "partner_id.country_id"
     ], axis=1, inplace=True)
 
-    #  for record in workbook.to_dict():
-    #  print(workbook.to_json(orient="records"))
-
-    #  print(workbook["check_in_date"])
-    # Forzar tipos de datos
-    # NOTA: Aquí no podemos usar un strformat por que los tipos de datos están combinados para esta columna, pero
-    # sí podemos pedirle a pandas que infiera la fecha.
-    #  print(workbook["invoice_date"])
+    data = workbook.to_dict(orient="records")
 
 
 if __name__ == "__main__":
-
-    xlsx2jsonPandas(XLSX_PATH)
+    xlsx2json(XLSX_PATH)
